3c37e5cd9e9a521a3695aafd3f6e38d2a26b10c9
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-04-11 14:14:25 -0300

    feat: added meal detail

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealDetail from './screens/MealDetailScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.Fragment>
+      <StatusBar style='light'/>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+           headerStyle: { backgroundColor: '#351401' },
+           headerTintColor: '#FFF',
+           contentStyle: { backgroundColor: '#3F2F25' }
+        }}>
+          <Stack.Screen 
+            name='MealsCategories' 
+            component={CategoriesScreen}
+            options={{ title: 'All Categories' }}
+          />
+          <Stack.Screen 
+            name='MealsOverview' 
+            component={MealsOverviewScreen}
+           /*  options={({ route, navigation }) => { 
+              const catId = route.params.categoryId;
+              return {
+                title: catId
+              };
+            }} */
+          />
+          <Stack.Screen
+            name='MealDetail'
+            component={MealDetail}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,108 @@
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native';
+import MealDetails from '../components/MealDetails';
+import { MEALS } from '../data/dummy-data';
+
+function MealDetail({ route }) {
+
+    const mealId = route.params.mealId;
+    const selectedMeal = MEALS.find((meal) => meal.id = mealId);
+
+    return(
+       <ScrollView style={styles.rootContainer}>
+            <Image style={styles.image} source={{uri: selectedMeal.imageUrl}}/>
+            <Text style={styles.title}>{selectedMeal.title}</Text>
+            <MealDetails 
+                duration={selectedMeal.duration}
+                complexity={selectedMeal.complexity}
+                affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
+            />
+          <View style={styles.listOuterContiner}>
+            <View style={styles.listContainer}>
+                <View style={styles.subtitleContainer}>
+                        <Text style={styles.subtitle}>Ingredients</Text>
+                    </View>
+                    {selectedMeal.ingredients.map((ingredient) => (
+                        <View style={styles.listItem}>
+                            <Text style={styles.itemText} key={ingredient}>{ingredient}</Text>
+                        </View>
+                    ))}
+
+                    <View style={styles.subtitleContainer}>
+                        <Text style={styles.subtitle}>Steps</Text>
+                    </View>
+                    {selectedMeal.steps.map((step) => (
+                        <View style={styles.listItem}>
+                            <Text style={styles.itemText} key={step}>{step}</Text>
+                        </View>
+                    ))}
+            </View>
+          </View>
+       </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    rootContainer: {
+        marginBottom: 24
+    }, 
+
+    image: {
+        width: '100%',
+        height: 350
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: '#FFF'
+    },
+    detailText: {
+        color: '#FFF'
+    },
+
+    subtitle: {
+        color: '#E2B497',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+
+    subtitleContainer: {
+        margin: 4,
+        padding: 6,
+        marginHorizontal: 24,
+        marginVertical: 4,
+        borderBottomColor: '#E2B497',
+        borderBottomWidth: 2
+    },
+
+    listItem: {
+        borderRadius: 6,
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        marginVertical: 4,
+        marginHorizontal: 12,
+        backgroundColor: '#E2B497'
+    },
+
+    itemText: {
+        color: '#351401',
+        textAlign: 'center'
+    },
+
+    listContainer: {
+        width: '80%',
+    },
+
+    listOuterContiner: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+
+});
+
+export default MealDetail;
